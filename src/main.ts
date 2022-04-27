ee82d06a6eb8bf79f4b1df1359c9ff793d039277
Makoto Ishida <70257274+makotoishida@users.noreply.github.com>
2022-04-26 20:32:16 -1000

    Implement scales etc.

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ function render(t: Date) {
       <svg version="1.1" baseProfile="full" width="400" height="400" xmlns="http://www.w3.org/2000/svg">
         <rect width="100%" height="100%" fill="#ffc" />
         <circle cx="200" cy="200" r="190" fill="#fefefe" stroke="#888" />
+        ${drawScale()}
         ${timeDigital(t)}
         ${drawHand(16, 128, degH, '#1010e0d0')}
         ${drawHand(10, 158, degM, '#4040e0d0')}
@@ -30,6 +31,23 @@ function render(t: Date) {
       </svg>
     </div>
   `
+}
+
+function drawScale() {
+  const scales = [...new Array(12)]
+  return scales
+    .map((_, index) => {
+      const deg = (360 * index) / 12 - 90
+      const x = 200 + Math.cos((deg * Math.PI) / 180) * 140
+      const y = 200 + Math.sin((deg * Math.PI) / 180) * 140 + 12
+      return `<rect x="-3" y="${
+        -18 - 165
+      }" rx="4" ry="4" width="5" height="18" fille="#000" style="transform: translate(50%,50%) rotate(${deg}deg)" />
+      <text x="${x}" y="${y}" font-size="28" text-anchor="middle" fill="#404040f0">${
+        index === 0 ? 12 : index
+      }</text>`
+    })
+    .join('')
 }
 
 function drawHand(w: number, h: number, deg: number, color: string) {
@@ -41,7 +59,7 @@ function drawHand(w: number, h: number, deg: number, color: string) {
 }
 
 function timeDigital(d: Date) {
-  return `<text x="200" y="280" font-size="40" text-anchor="middle" fill="#88c">
+  return `<text x="200" y="280" font-size="40" text-anchor="middle" fill="#404040e0">
     ${timeStr(d)}
   </text>`
 }
